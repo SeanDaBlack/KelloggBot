8fc7027ac691f8e9c1b5d2ef6579e448b03a65cc
jg <57115019+joeyagreco@users.noreply.github.com>
2021-12-10 16:40:26 -0600

    moved xpaths to constants file

--- main.py
+++ main.py
@@ -15,15 +15,6 @@ fake = Faker()
 chromedriver_location = "./chromedriver"
 print = functools.partial(print, flush=True)
 
-# data2 = {
-#     'resume': '//*[@id="49:_file"]',
-#     'addy': '//*[@id="69:_txtFld"]',
-#     'city': '//*[@id="73:_txtFld"]',
-#     'zip': '//*[@id="81:_txtFld"]',
-#     'job': '//*[@id="101:_txtFld"]',
-#     'salary': '//*[@id="172:_txtFld"]',
-#     'country': '//*[@id="195:_select"]'
-# }
 data = {
     'email': '//*[@id="fbclc_userName"]',
     'email-retype': '//*[@id="fbclc_emailConf"]',
