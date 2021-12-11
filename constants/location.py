3a69412193113244c7488be97f11d4b302034c55
jg <57115019+joeyagreco@users.noreply.github.com>
2021-12-10 18:01:17 -0600

    moved constant file constants around

--- constants/location.py
+++ constants/location.py
@@ -11,5 +11,12 @@ CITIES_TO_ZIP_CODES = {
     'Memphis': ['38116', '38118', '38122', '38127', '38134', '38103'],
 }
 
+CITIES_TO_URLS = {
+    'Lancaster': 'https://jobs.kellogg.com/job/Lancaster-Permanent-Production-Associate-Lancaster-PA-17601/817684800/#',
+    'Omaha': 'https://jobs.kellogg.com/job/Omaha-Permanent-Production-Associate-Omaha-NE-68103/817685900/z',
+    'Battle Creek': 'https://jobs.kellogg.com/job/Battle-Creek-Permanent-Production-Associate-Battle-Creek-MI-49014/817685300/',
+    'Memphis': 'https://jobs.kellogg.com/job/Memphis-Permanent-Production-Associate-Memphis-TN-38114/817685700/'
+}
+
 COUNTRY_CODE_US = 'US'
 FULL_NAME_US = 'United States'
--- constants/urls.py
+++ constants/urls.py
@@ -1,9 +1,2 @@
-CITIES_TO_URLS = {
-    'Lancaster': 'https://jobs.kellogg.com/job/Lancaster-Permanent-Production-Associate-Lancaster-PA-17601/817684800/#',
-    'Omaha': 'https://jobs.kellogg.com/job/Omaha-Permanent-Production-Associate-Omaha-NE-68103/817685900/z',
-    'Battle Creek': 'https://jobs.kellogg.com/job/Battle-Creek-Permanent-Production-Associate-Battle-Creek-MI-49014/817685300/',
-    'Memphis': 'https://jobs.kellogg.com/job/Memphis-Permanent-Production-Associate-Memphis-TN-38114/817685700/'
-}
-
 CHROMEDRIVER_PATH = './chromedriver'
 RESUME_PATH = '/src/resume.png'
